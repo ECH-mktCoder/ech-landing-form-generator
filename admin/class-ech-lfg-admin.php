a7de850d40a3d02c06ad32b01b8d2c1a80f82398
Toby Wong <tobywong@prohaba.com>
2023-07-31 16:21:36 +0800

    Apply css/js file only in this plugin admin page

--- admin/class-ech-lfg-admin.php
+++ admin/class-ech-lfg-admin.php
@@ -61,19 +61,10 @@ class Ech_Lfg_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Ech_Lfg_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Ech_Lfg_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+		if ( (isset($_GET['page']) && $_GET['page'] == 'ech_lfg_general_settings') || (isset($_GET['page']) && $_GET['page'] == 'ech_lfg_recaptcha') ) {
+			wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/ech-lfg-admin.css', array(), $this->version, 'all' );
+		}
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/ech-lfg-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -84,19 +75,10 @@ class Ech_Lfg_Admin {
 	 */
 	public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Ech_Lfg_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Ech_Lfg_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+		if ( (isset($_GET['page']) && $_GET['page'] == 'ech_lfg_general_settings') || (isset($_GET['page']) && $_GET['page'] == 'ech_lfg_recaptcha') ) {
+			wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ech-lfg-admin.js', array( 'jquery' ), $this->version, false );
+		}
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ech-lfg-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
