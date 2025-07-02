a4890732c876423d5676158da9be262c8f3fde87
Rowan Prohaba <rowanchang@prohaba.com>
2025-07-02 12:51:22 +0800

    default tel prefix value

--- public/class-ech-lfg-public.php
+++ public/class-ech-lfg-public.php
@@ -607,7 +607,7 @@ class Ech_Lfg_Public
 			<div class="form_row" data-ech-field="telPrefix">
 				<select  class="form-control" name="telPrefix" id="tel_prefix" style="width: 100%;" required >
 					<option disabled="" selected="" value="">*請選擇</option>
-					<option value="+852">+852</option>
+					<option value="+852" selected>+852</option>
 					<option value="+853">+853</option>
 					<option value="+86">+86</option> 
 				</select>
